0df30517a9bdedb79b6c513cbd2e212897b2e09c
caterinator <caterina.torsello@outlook.com>
2025-07-12 11:13:36 +0200

    Update example_for_pylint.py

--- example_for_pylint.py
+++ example_for_pylint.py
@@ -1,11 +1,17 @@
+"""Modul-Dokumentationsstring (Modul-Docstring): 
+    Kommentar in Python, der direkt am Anfang einer .py-Datei steht und in dreifachen Anführungszeichen geschrieben wird. 
+    Er dient dazu, das Modul (also die Datei) zu beschreiben: Was es tut, wofür es gedacht ist und ggf. wie man es benutzt.
+"""
 def add_numbers(a, b):
     """Add two numbers and return the result."""
     return a + b
 
 def say_hello(name):
+    """Say hello to someone"""
     print("Hello, " + name)
 
 def main():
+    """Print the result"""
     sum_result = add_numbers(5, 7)
     print(f"The sum is {sum_result}")
 
